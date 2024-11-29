b94a29c7cbce6a1a171da3c3771296f34c60ebde
Almaz <Zickam@mail.ru>
2024-11-29 12:34:45 +0500

    feat: styles added a little bit and more info on weather

---
+++ README.md
@@ -0,0 +1,4 @@
+# Weather App
+Автор: Сулейманов Алмаз
+## Использованные библиотеки
+* [Yandex Maps Javascript API](https://yandex.com/dev/jsapi30/doc/en/), npm пакет которой называется **ol**
--- script.js
+++ script.js
@@ -34,14 +34,23 @@ function updateCoordsFromMap(){
 }
 
 async function update() {
-    const latitude_input = document.getElementById("inputLatitude").value
-    const longitude_input = document.getElementById("inputLongitude").value
-    
-    console.log(map)
-    map.update({location: {center:[longitude_input, latitude_input] }})
-    console.log(map)
+    const latitude_input = document.getElementById("inputLatitude")
+    const longitude_input = document.getElementById("inputLongitude")
+    const latitude = latitude_input.value
+    const longitude = longitude_input.value
 
-    const url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${latitude_input},${longitude_input}&aqi=no`
+    if (!(-90 <= latitude && latitude <= 90) | !latitude){
+        alert("Incorrect coordinates! Latitude should be placed between -90 and 90 degrees")
+        return
+    }
+    if (!(-180 <= longitude && longitude <= 180) | !longitude){
+        alert("Incorrect coordinates! Longitude should be placed between -180 and 180 degrees")
+        return
+    }
+    
+    map.update({location: {center:[longitude, latitude] }})
+
+    const url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${latitude},${longitude}&aqi=no`
     
     let response = await fetch(url)
     let json_response = await response.json()
@@ -49,12 +58,19 @@ async function update() {
     console.log("API response:", json_response)
 
     if (json_response.error && json_response.error.code == 1006) {
-        alert("Incorrect coordinates specified")
+        alert("No nearest town was found for this coordinates so impossible to determine the weather here")
     }
 
     const temperature_display = document.getElementById("celsius")
     temperature_display.textContent = json_response.current.temp_c + " °C"
     const town_display = document.getElementById("town")
-    town_display.textContent = "Town: " + json_response.location.name
-    
+    town_display.textContent = "Town: " + json_response.location.country + ", " + json_response.location.name
+    const icon_display = document.getElementById("icon")
+    icon_display.src = json_response.current.condition.icon
+    const localtime_display = document.getElementById("localtime")
+    localtime_display.textContent = "Local time: " + json_response.location.localtime
+    const windforce_display = document.getElementById("windForce")
+    windforce_display.textContent = "Wind speed: " + json_response.current.wind_kph + "km/h"
+    const winddirection_display = document.getElementById("windDirection")
+    winddirection_display.textContent = "Wind direction: " + json_response.current.wind_dir
 }
