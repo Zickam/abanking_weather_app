e1945209c4d6c368111af57f1eaf5a699896a0bd
Almaz <Zickam@mail.ru>
2024-11-30 13:20:59 +0500

    feat: multiple widgets

--- script.js
+++ script.js
@@ -2,18 +2,28 @@
 
 const API_KEY="6f3a5aa8273a4f69b1a100623242711"
 
-let lat = 56.838011
-let lon = 60.597474
+const lat = 56.838011
+const lon = 60.597474
 
-var map;
+// [related_main_menu_div, related_map]
+let widgets = {
 
-initMap()
+}
 
-async function initMap(){
+String.prototype.format = function() {
+    a = this;
+    for (k in arguments) {
+      a = a.replace("{" + k + "}", arguments[k])
+    }
+    return a
+}
+
+
+async function initMap(widget_number){
     await ymaps3.ready
     const {YMap, YMapDefaultSchemeLayer} = ymaps3;
     map = new YMap(
-    document.getElementById('map'),
+    document.getElementById('map' + widget_number),
     {
         location: {
             center: [lon, lat],
@@ -22,20 +32,44 @@ async function initMap(){
         }
     );
     map.addChild(new YMapDefaultSchemeLayer());
-    updateCoordsFromMap()
+    return map
 }
 
-function updateCoordsFromMap(){
-    lat = map.center[1]
-    lon = map.center[0]
+function getMainMenuIDOfElement(elem) {
+    while (elem.className != "mainMenu"){
+        elem = elem.parentElement
+    }
+    let length = elem.id.length
+    return elem.id.substring(8, length + 1)
+}
+
+function getWidgetOfElem(elem) {
+    while (elem.className != "mainMenu"){
+        elem = elem.parentElement
+    }
+    return elem
+}
+
+function updateCoordsFromMap(elem){
+    let widget_number = getMainMenuIDOfElement(elem)
+    let widget = getWidgetOfElem(elem)
+
+    const map = widgets[widget_number].map
     
-    document.getElementById("inputLatitude").value = lat
-    document.getElementById("inputLongitude").value = lon
+    const input_lat = widget.querySelector('#inputLatitude');
+    const input_lon = widget.querySelector('#inputLongitude');
+
+    input_lat.value = map.center[1]
+    input_lon.value = map.center[0]
 }
 
-async function update() {
-    const latitude_input = document.getElementById("inputLatitude")
-    const longitude_input = document.getElementById("inputLongitude")
+async function update(elem) {
+    let widget = getWidgetOfElem(elem)
+    let widget_number = getMainMenuIDOfElement(elem)
+    const latitude_input = widget.querySelector("#inputLatitude")
+    const longitude_input = widget.querySelector("#inputLongitude")
+    const map = widgets[widget_number].map
+
     const latitude = latitude_input.value
     const longitude = longitude_input.value
 
@@ -59,18 +93,62 @@ async function update() {
 
     if (json_response.error && json_response.error.code == 1006) {
         alert("No nearest town was found for this coordinates so impossible to determine the weather here")
+        return
     }
 
-    const temperature_display = document.getElementById("celsius")
+    const temperature_display = widget.querySelector("#celsius")
     temperature_display.textContent = json_response.current.temp_c + " °C"
-    const town_display = document.getElementById("town")
+    const town_display = widget.querySelector("#town")
     town_display.textContent = "Town: " + json_response.location.country + ", " + json_response.location.name
-    const icon_display = document.getElementById("icon")
+    const icon_display = widget.querySelector("#icon")
     icon_display.src = json_response.current.condition.icon
-    const localtime_display = document.getElementById("localtime")
+    const localtime_display = widget.querySelector("#localtime")
     localtime_display.textContent = "Local time: " + json_response.location.localtime
-    const windforce_display = document.getElementById("windForce")
+    const windforce_display = widget.querySelector("#windForce")
     windforce_display.textContent = "Wind speed: " + json_response.current.wind_kph + "km/h"
-    const winddirection_display = document.getElementById("windDirection")
+    const winddirection_display = widget.querySelector("#windDirection")
     winddirection_display.textContent = "Wind direction: " + json_response.current.wind_dir
 }
+
+async function addWidget() {
+    const body = document.getElementById("body")
+    let child = document.createElement("div");
+    child.className = "mainMenu"
+    let widget_number = Object.keys(widgets).length + 1;
+    child.id = "mainMenu" + (widget_number)
+    child.innerHTML = mainMenuHTML.format(widget_number)
+
+    body.appendChild(child)
+
+    map = await initMap(widget_number)
+
+    widgets[widget_number] = {"div": child, "map": map}
+
+    updateCoordsFromMap(child)
+    update(child)
+}
+
+
+const mainMenuHTML = `
+    <div class="info">
+        <div class="searchBox">
+            <input placeholder="latitude" id="inputLatitude"></input>
+        </div>
+        <div class="searchBox">
+            <input placeholder="longitude" id="inputLongitude"></input>
+        </div>
+        <div class="currentConditions">
+            <p id="celsius" class="condition"></p>
+            <p id="town" class="condition"></p>
+            <img id="icon" src="" class="condition">
+            <p id="localtime" class="condition"></p>
+            <p id="windForce" class="condition"></p>
+            <p id="windDirection" class="condition"></p>
+        </div>
+        <div class="controls">
+            <button onclick="update(this)">Get weather info</button>
+
+            <button onclick="updateCoordsFromMap(this)">Use coordinates from map</button>
+        </div>
+    </div>
+    <div class="map" id="map{0}"></div>`
